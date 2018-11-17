1f8a9877b310ef77bd052c3622f8ff6660b54676
shakti97 <shaktisingh230797@gmail.com>
2018-11-17 10:56:50 +0530

    build server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express=require('express');
+const app=express();
+const bodyParser=require('body-parser');
+const session=require('express-session');
+const path=require('path');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+const cors=require('./utils/cors.js');
+app.use(cors());
+const store=require('./utils/sessionHandling/sessionstore');
+app.use(session({
+store : store,
+    secret: 'shakti',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+
+        httpOnly: false,
+        secure: false,
+        maxAge: 1000*60*5
+    }
+
+}));
+
+app.use('/',app);
+
+app.listen(process.env.PORT || 8080,()=>{
+    console.log('Server Started');
+})
