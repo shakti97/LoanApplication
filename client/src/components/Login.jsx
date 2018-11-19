ffa5804a46dd47f78cee8dc99dfb4888ce7e7efa
shakti97 <shaktisingh230797@gmail.com>
2018-11-19 13:59:24 +0530

    refactor the login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,82 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
+import api from '../globalConst';
 
-class Login {
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: "", password: "" }
+    }
+    handleChange = (event) => {
+        event.preventDefault();
+        this.setState({ [event.target.name]: event.target.value }, () => { console.log(this.state) });
+
+    }
+    Login = (event) => {
+        event.preventDefault();
+        console.log('login');
+        let userDetails = this.state;
+        fetch(api + '/login', {
+            headers: { 'Content-Type': 'application/json'},
+            method: "POST",
+            body: JSON.stringify(userDetails),
+            credentials: 'include'
+        }).then((res) => {
+            res.json().then((data) => {
+                console.log(data);
+                localStorage.setItem("sessionId", data.sessionId);
+                localStorage.setItem('userId', data.userId);
+                if (data.role === 'Customer') {
+                    this.props.history.push('/customer');
+                } else if (data.role === "Lender") {
+                    this.props.history.push('/lender');
+                }
+            })
+        }).catch((res) => {
+            console.log('error while fetching ', res);
+        })
+    }
+    loginWithFb=()=>{
+        console.log('login with fb ');
+        fetch(api+'/auth/facebook',{
+
+        }).then((res)=>{
+            res.json().then((data)=>{
+                console.log('data ',data);
+            })
+        }).catch((res)=>{
+            console.log('error in fb login ');
+        })
+    }
     render() {
         return (
             <div className="log-form">
-                <h2>Login to your account</h2>
-                <form >
+                <form onSubmit={this.Login}>
                     <div className="imgcontainer">
-                        <img src="img_avatar2.png" alt="Avatar" className="avatar" />
+                        <img src="https://banner2.kisspng.com/20180425/zye/kisspng-computer-icons-avatar-user-login-5ae149b20c8348.1680096815247139060513.jpg" alt="Avatar" className="avatar" />
                     </div>
 
                     <div className="container">
-                        <label for="uname"><b>Username</b></label>
-                        <input type="text" placeholder="Enter Username" name="uname" required />
+                        <label ><b>Email</b></label>
+                        <input type="text" placeholder="Enter Email" name="email" onChange={this.handleChange} required />
 
-                        <label for="psw"><b>Password</b></label>
-                        <input type="password" placeholder="Enter Password" name="psw" required />
+                        <label ><b>Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="password" onChange={this.handleChange} required />
 
-                        <button type="submit">Login</button>
-                        
+                        <button type='submit' >Login</button>
+
                     </div>
 
-                    <div className="container" style="background-color:#f1f1f1">
-                        <Link to='/signUp'><button type="button" className="cancelbtn">Sign Up</button></Link>
+                    <div className="container" style={{ "backgroundColor": "#f1f1f1" }}>
+                        <Link to='/signUp'><span className="btn btn-warning">Sign Up</span></Link>
+                        <a href={api+"/auth/facebook"}><span className='btn btn-primary'>  Login With Facebook</span></a>
 
-                        
                     </div>
                 </form>
-                <button className='btn btn-primary'>Login With Facebook</button>
-            </div> 
+
+            </div>
         )
     }
 }
+export default Login;
