78084447bb6f32797dc6a9fb809cac1045b80680
shakti97 <shaktisingh230797@gmail.com>
2018-11-19 18:46:56 +0530

    Restricting illegal Access of Admin Component

--- client/src/components/admin.jsx
+++ client/src/components/admin.jsx
@@ -1,75 +1,83 @@
 import React, { Component } from 'react';
 import api from '../globalConst';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-class AdminPanel extends Component{
-    constructor(props){
+class AdminPanel extends Component {
+    constructor(props) {
         super(props);
-        this.state={loans : []};
+        this.state = { loans: [] };
     }
-    Logout=()=>{
+    Logout = () => {
         console.log('Logout');
         localStorage.clear();
         this.props.history.push('/');
     }
-    componentWillMount(){
+    fetchLoans() {
         console.log('lender component will mount ');
-        fetch(api+'/fetchAllLoans',{
-            method : "GET",
-            headers :{
-                authToken : localStorage.getItem('sessionId'),
-                userId : localStorage.getItem('userId')
+        fetch(api + '/fetchAllLoans', {
+            method: "GET",
+            headers: {
+                authToken: localStorage.getItem('sessionId'),
+                userId: localStorage.getItem('userId')
             }
-        }).then((res)=>{
-            res.json().then((data)=>{
+        }).then((res) => {
+            res.json().then((data) => {
                 console.log(data);
-                if(data.loans){
+                if (data.loans) {
                     this.setState({
-                        loans :data.loans
+                        loans: data.loans
                     })
                 }
-                else if(!data.isAuth){
+                else if (!data.isAuth) {
                     this.Logout();
                 }
             })
-        }).catch((err)=>{
-            console.log('err in fetch all loans ',err);
+        }).catch((err) => {
+            console.log('err in fetch all loans ', err);
         })
     }
-    render(){
-        return(
+    componentWillMount() {
+        console.log('this.props.location', this.props.location);
+        let routes = localStorage.getItem('routes');
+        let path = this.props.location.pathname;
+        var legal = routes.includes(path)
+        legal && this.fetchLoans();
+        !legal && this.Logout();
+    }
+    render() {
+        return (
             <div>
                 <div>
-                <div style={{ float: 'right' ,marginRight : "2em"}}> <button className="btn btn-danger" onClick={this.Logout}>LogOut</button> </div>
+                    <div style={{ float: 'right', marginRight: "2em" }}> <button className="btn btn-danger" onClick={this.Logout}>LogOut</button> </div>
 
-                <h4>Admin Home</h4>
-                <br />
-                <div>
-                    <center><table className="paleBlueRows">
-                        <thead>
-                            <tr>
-                                <th>Amount</th>
-                                <th>LoanReason</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.loans.map((loan, i) =>
-                                <tr key={loan.loanId}>
-                                    <td>{loan.amount}</td>
-                                    <td>{loan.LoanReason}</td>
-                                    {loan.status==="Pending" && <td><button className='btn btn-warning'>Pending</button></td>}
-                                    {loan.status==="Rejected" && <td><button className='btn btn-danger'>Rejected</button></td>}
-                                    {loan.status==="Approved" && <td><button className='btn btn-success'>Approved</button></td>}
+                    <h4>Admin Home</h4>
+                    <br />
+                    <div>
+                        <center><table className="paleBlueRows">
+                            <thead>
+                                <tr>
+                                    <th>Amount</th>
+                                    <th>LoanReason</th>
+                                    <th>Status</th>
                                 </tr>
-                            )}
-                        </tbody>
-                    </table>
-                    </center>
-                    
-                </div>
+                            </thead>
+                            <tbody>
+                                {this.state.loans.map((loan, i) =>
+                                    <tr key={loan.loanId}>
+                                        <td>{loan.amount}</td>
+                                        <td>{loan.LoanReason}</td>
+                                        {loan.status === "Pending" && <td><button className='btn btn-warning'>Pending</button></td>}
+                                        {loan.status === "Rejected" && <td><button className='btn btn-danger'>Rejected</button></td>}
+                                        {loan.status === "Approved" && <td><button className='btn btn-success'>Approved</button></td>}
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                        </center>
 
-            </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
