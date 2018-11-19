e4a236b52160950fa13a1f9a75acd9cbc88b2e60
shakti97 <shaktisingh230797@gmail.com>
2018-11-19 13:56:58 +0530

    Refactor cors to allow origin

--- server/utils/cors.js
+++ server/utils/cors.js
@@ -1,6 +1,7 @@
 const cors = (request, response, next)=>{
-    response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log("cors",request);
+    response.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,userId,authToken");
     response.header('Access-Control-Allow-Credentials','true');
     response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     
