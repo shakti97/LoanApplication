743ff1d7c3b30af8ce24bc970e0c4a0ab6c10d69
shakti97 <shaktisingh230797@gmail.com>
2018-11-19 17:46:41 +0530

    Added admin id

---
+++ server/database/adminSignUp.js
@@ -0,0 +1,3 @@
+const UserOperation=require('./UserOperations');
+userObject={ name : "admin" ,email : "adminsingh@gmail.com",password : "admin123",role : "Admin"};
+UserOperation.adminSignUp(userObject);
